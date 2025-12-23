bb1aae0aa19ef31d2ab8d513ec03c3d87d2c6ad7
Jyotish Kumar <jkshaw@Jyotishs-MacBook-Pro.local>
2025-12-23 17:01:11 +0530

    practice session 15

---
+++ math/spiral_order.py
@@ -0,0 +1,53 @@
+"""
+Spiral Matrix
+
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+"""
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+        top, bottom, left, right = 0, m - 1, 0, n - 1
+        result = []
+
+        # Iterate until all elements are printed
+        while top <= bottom and left <= right:
+            # Print top row from left to right
+            for i in range(left, right + 1):
+                result.append(matrix[top][i])
+            top += 1
+
+            # Print right column from top to bottom
+            for i in range(top, bottom + 1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            # Print bottom row from right to left (if exists)
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+
+            # Print left column from bottom to top (if exists)
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+        return result
